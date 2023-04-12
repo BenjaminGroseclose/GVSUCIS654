5d41c3a586b6204920dd47519f87e829a86b6a07
Ben Groseclose <ben.groseclose01@gmail.com>
2023-04-11 20:39:42 -0400

    py rework

--- server-determination.py
+++ server-determination.py
@@ -1,42 +1,62 @@
-import os
+from pythonping import ping
 
-hostname = "96.7.137.208"
-response = os.system("ping /n 5 " + hostname)
+class Server:
+    """Represents severs provided for the server viablilty application"""
+    def __init__(self, ipAddress: str, name: str) -> None:
+       self._ipAddress = ipAddress
+       self._name = name
+      
+    def setResponse(self, min, avg, max, success):
+      self._min = min
+      self._max = max
+      self._avg = avg
+      self._success = success
+    
+    @property
+    def ipAddress(self) -> str:
+      return self._ipAddress
+  
+    @property
+    def name(self) -> str:
+      return self._name
+    
+    @property
+    def min(self) -> str:
+      return self._min
+    
+    @property
+    def avg(self) -> str:
+      return self._avg
+    
+    @property
+    def max(self) -> str:
+      return self._max
+    
+    @property
+    def success(self) -> bool:
+      return self._success
+    
+# 1200 payload size is relatable to average game payalods
+emptyPaylod = bytearray(1200)
 
-print("United States East")
-print(response)
+servers = [
+  Server("96.7.137.208", "United States East"),
+  Server("70.191.189.99", "United States West"),
+  Server("77.74.193.94", "United Kingdom"),
+  Server("45.235.47.246", "Brazil"),
+  Server("185.238.200.7", "Russia"),
+  Server("92.173.162.22", "France")
+]
 
-hostname = "United States West"
-response = os.system("ping /n 5 " + hostname)
+for server in servers:
+  print(server.name)
+  result = ping(server.ipAddress, payload=emptyPaylod, count=5, interval=1, verbose=True)
 
-print("United Kingdom")
-print(response)
-
-
-hostname = "77.74.193.94"
-response = os.system("ping /n 5 " + hostname)
-
-print("United Kingdom")
-print(response)
-
-
-hostname = "45.235.47.246"
-response = os.system("ping /n 5 " + hostname)
-
-print("Brazil")
-print(response)
-
-
-hostname = "185.238.200.7"
-response = os.system("ping /n 5 " + hostname)
-
-print("Russia")
-print(response)
-
-
-hostname = "92.173.162.22 "
-response = os.system("ping /n 5 " + hostname)
-
-print("France")
-print(response)
+  server.setResponse(result.rtt_min_ms, result.rtt_avg_ms, max=result.rtt_max_ms, success=result.success(option=3))
+  print(f'Min: {server.min}')
+  print(f'Avg: {server.avg}')
+  print(f'Max: {server.max}')
+  print(f'Success: {server.success}')
+  print('---------------')
+  print('\n')
 
